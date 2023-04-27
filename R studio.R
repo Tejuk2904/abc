24464bb686ed48c13ecc00257599ed4d12578f31
Tejuk2904 <131948532+Tejuk2904@users.noreply.github.com>
2023-04-27 12:27:23 +0530

    Add files via upload

---
+++ R studio.R
@@ -0,0 +1,59 @@
+print ('Hi')
+
+#variables 
+a<- 2
+b<- 8
+c<- a+b
+c
+print(c)
+
+#numeric 
+a<- 5.5
+class(a)
+
+#character
+b<- "pune"
+class(b)
+
+#logical 
+c<- 10>5
+class(c)
+
+#R arithmetic operators
+a+b
+a-b
+a*b
+a/b
+
+# e is always 2.71
+exp(1)
+exp(2)
+sqrt(9)
+
+#vector
+age<- c(25,20,40,30,20)
+age 
+lenght(age)
+mean(age)
+median(age)
+summary(age)
+age*12
+
+vect<-c('a','b','c')
+summary(c)
+
+x<- c(2,0,0,3)
+x[1]
+x[-1]
+y<-x[1]
+y
+y<-x[-1]
+y
+
+#index
+age<- c(10,20,30,40,50)
+age[1]
+age[2:3]
+age[c(1,3,5)]
+
+#dataframes
